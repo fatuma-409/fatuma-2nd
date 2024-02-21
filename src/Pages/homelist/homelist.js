d81356b0be8253fdafcbe8f61f73de79241bafb0
lila-ana <gelilat1216@gmail.com>
2024-02-21 16:59:51 +0300

    few adjustments

--- src/Pages/homelist/homelist.js
+++ src/Pages/homelist/homelist.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeList = ({ title, fetchurl }) => {
   const [videos, setVideos] = useState([]);
@@ -17,29 +17,31 @@ const HomeList = ({ title, fetchurl }) => {
     fetchData();
   }, [fetchurl]);
 
-  console.log(videos, "checking");
+  console.log(videos, fetchurl, "checking");
 
   return (
-   
-
     <>
-    <h1 className='text-blue-800 bg-black-200 opacity-100 sm:font-3xl md-font-5xl'>{title}</h1>
-    <div className='relative flex item-center'>
-      <div className='slider'>
-        <div className='w-full h-full'>
-          {videos?.map((item, id) => (
-            <div key={id} className='w-[160px] sm:w-[200px]'>
-            <img className='w-full ' src={`https://api.themoviedb.org/3/${item.backdrop_path}`} alt={item.title} />
-            </div>
-          ))}
+      <h1 className="text-blue-800 bg-black-200 opacity-100 sm:font-3xl md-font-5xl">
+        {title}
+      </h1>
+      <div className="relative flex item-center">
+        <div className="slider">
+          <div className="w-full h-full">
+            {videos?.map((item, id) => (
+              <div key={id} className="w-[160px] sm:w-[200px]">
+                <img
+                  className="w-full "
+                  // src="https://static.toiimg.com/photo/msid-53891743,width-96,height-65.cms"
+                  src={`https://image.tmdb.org/t/p/original${item?.backdrop_path}`}
+                  alt={item.title}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </>
-      );
-    };
-    
-    
-
+    </>
+  );
+};
 
 export default HomeList;
